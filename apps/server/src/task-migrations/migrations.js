eefc435677d8d25e27eed496332462094e2309aa
spectralgo <florian.renard@spectralgo.com>
2025-10-29 03:16:14 +0000

    DB: Format migration files and add JSON import assertions

--- apps/server/src/task-migrations/migrations.js
+++ apps/server/src/task-migrations/migrations.js
@@ -1,10 +1,9 @@
-import journal from './meta/_journal.json';
-import m0000 from './0000_warm_jean_grey.sql';
+import m0000 from "./0000_warm_jean_grey.sql";
+import journal from "./meta/_journal.json" with { type: "json" };
 
-  export default {
-    journal,
-    migrations: {
-      m0000
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+  },
+};
